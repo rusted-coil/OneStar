955127043ff802c52239a202d33b8201434f1643
rusted-coil <rusted.coil@gmail.com>
2020-02-05 16:47:01 +0900

    CUIテストデータ更新

--- OneStarCUI/Program.cs
+++ OneStarCUI/Program.cs
@@ -8,15 +8,23 @@ namespace OneStarCUI
 		static void Main(string[] args)
 		{
 			// 適当に計算したりするテスト用コンソールです
-
+			// 0x123イベントレイド
+			// マホミル★3
+			// 4匹目3V 31 28 31  4 31 11 うっかりや 夢 ♀固定 個性1 うたれづよい
+			// マホミル★4
+			// 4匹目4V 31 10 31 31 31 27 ひかえめ 特性1 ♀固定 個性1 うたれづよい
+			// マホミル★3
+			// 5匹目3V 27 21 31 31 18 31 のうてんき 特性1 ♀固定 個性2 うたれづよい
+			// マホミル★3
+			// 6匹目3V 31 31  3 17 31  6 さみしがり 特性1 ♀固定 個性3 みえっぱり
 			SeedSearcher searcher = new SeedSearcher(SeedSearcher.Mode.Cuda35);
 			SeedSearcher.CudaInitialize();
-			SeedSearcher.SetCudaCondition(0, 31, 27, 3, 19, 18, 31, 1, 15, 3, false, 3, 2);
-			SeedSearcher.SetCudaCondition(1, 31, 9, 31, 31, 15, 31, 1, 18, 2, false, 3, 4);
-			SeedSearcher.SetCudaCondition(2, 31, 31, 31, 1, 31, 27, 1, 6, 2, true, 4, 4);
-			SeedSearcher.SetCudaCondition(3, 31, 2, 23, 31, 31, 31, 1, 15, 2, true, 4, 4);
-			SeedSearcher.SetCudaTargetCondition6(27, 3, 19, 18, 9, 15);
-			searcher.CudaLoopPartition = 8;
+			SeedSearcher.SetCudaCondition(0, 31, 28, 31,  4, 31, 11, 2, 19, 2, true, 4, 3);
+			SeedSearcher.SetCudaCondition(1, 31, 10, 31, 31, 31, 27, 0, 15, 2, true, 4, 4);
+			SeedSearcher.SetCudaCondition(2, 27, 21, 31, 31, 18, 31, 0,  9, 2, true, 4, 3);
+			SeedSearcher.SetCudaCondition(3, 31, 31,  3, 17, 31,  6, 0,  1, 5, true, 4, 3);
+			SeedSearcher.SetCudaTargetCondition5(28, 4, 11, 10, 27);
+			searcher.CudaLoopPartition = 0;
 
 			Console.WriteLine("計算中...");
 
