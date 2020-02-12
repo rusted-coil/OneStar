311a490f6a9889c1a2fd4d697391bfaa3f6b71f2
rusted-coil <rusted.coil@gmail.com>
2020-02-12 11:08:58 +0900

    バージョン更新

--- OneStar/Form1.Designer.cs
+++ OneStar/Form1.Designer.cs
@@ -3158,7 +3158,7 @@
 			this.MainMenuStrip = this.menuStrip1;
 			this.MaximizeBox = false;
 			this.Name = "MainForm";
-			this.Text = "1-Star Seed Search v1.07b";
+			this.Text = "1-Star Seed Search v1.07c";
 			this.f_GroupBoxPokemon_1.ResumeLayout(false);
 			this.f_GroupBoxPokemon_1.PerformLayout();
 			this.f_GroupBoxStatus1.ResumeLayout(false);
