23ebaec9ebbc84634fed9b94a3272e19afefef2c
rusted_coil <rusted.coil@gmail.com>
2020-06-17 19:28:23 +0900

    レイドリストコンバータを追加

---
+++ DenListConverter/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PKHeX.Core;
+
+namespace DenListConverter
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+		}
+	}
+}
