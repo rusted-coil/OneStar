bce71042194f2ae7e2c35f95c42141a3a82bbaf4
rusted-coil <rusted.coil@gmail.com>
2020-06-22 19:26:24 +0900

    [v1.08e] 闘心の洞窟DとEのデータが逆になっていた不具合を修正

--- OneStar/Form1.Designer.cs
+++ OneStar/Form1.Designer.cs
@@ -3158,7 +3158,7 @@
 			this.MainMenuStrip = this.menuStrip1;
 			this.MaximizeBox = false;
 			this.Name = "MainForm";
-			this.Text = "1-Star Seed Search v1.08d";
+			this.Text = "1-Star Seed Search v1.08e";
 			this.f_GroupBoxPokemon_1.ResumeLayout(false);
 			this.f_GroupBoxPokemon_1.PerformLayout();
 			this.f_GroupBoxStatus1.ResumeLayout(false);
